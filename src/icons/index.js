84a8bcc9dbc7f1270a9c7860fa28130fc7378655
pratikbrowserstack <pratik.v@browserstack.com>
2024-05-04 18:40:36 +0530

    Necessary icons added

---
+++ src/icons/index.js
@@ -0,0 +1,5 @@
+import ArrowDownIcon from "./ArrowDownIcon";
+import ArrowRightIcon from "./ArrowRightIcon";
+import FileIcon from "./FileIcon";
+
+export { ArrowDownIcon, ArrowRightIcon, FileIcon };
